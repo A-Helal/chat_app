268d7ab5795e557b9b50102d3ace02da2796279b
A-Helal <adbelrahman21-00303@student.eelu.edu.eg>
2024-10-18 20:30:29 +0300

    change paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:chat_train_app/features/login_screen/presentation/views/screens/login_screen.dart';
-import 'package:chat_train_app/features/splash_screen/presentation/views/widgets/splash_screen.dart';
+import 'package:chat_train_app/features/splash_screen/presentation/views/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
